bb18f4c22627564dbde9845ab801bc8d9f9067a7
Ian Sanders <iansanders@mail.usf.edu>
2019-10-10 21:05:08 -0400

    Format and add comments

--- code/main.py
+++ code/main.py
@@ -13,45 +13,52 @@ keys_results = data_exporting.OutputSheet([grid_i.Field.TEST_FORM_CODE])
 app = tk.Tk()
 input_directory = file_handling.prompt_folder(
     "Select a folder to import scans from")
-images = file_handling.filter_images(
+image_paths = file_handling.filter_images(
     file_handling.list_file_paths(input_directory))
 output_directory = file_handling.prompt_folder(
     "Select a folder to save results in")
 app.destroy()
 
-for image_path in images:
+for image_path in image_paths:
     image = image_utils.get_image(image_path)
     prepared_image = image_utils.prepare_scan_for_processing(image)
 
+    # Find the corners, skipping the image on failure
     try:
         corners = corner_finding.find_corner_marks(prepared_image)
     except corner_finding.CornerFindingError:
         print(f"{image_path.name}: Can't find corners.")
         continue
-    grid = grid_r.Grid(corners, grid_i.GRID_HORIZONTAL_CELLS, grid_i.GRID_VERTICAL_CELLS, prepared_image)
 
-    last_name = grid_r.read_field_as_string(grid_i.Field.LAST_NAME, grid)
-    field_data: typing.Dict[grid_i.Field, str] = {}
+    # Establish a grid
+    grid = grid_r.Grid(corners, grid_i.GRID_HORIZONTAL_CELLS,
+                       grid_i.GRID_VERTICAL_CELLS, prepared_image)
 
-    if last_name == grid_i.KEY_LAST_NAME:
-        form_code_field = grid_i.Field.TEST_FORM_CODE
-        field_data[form_code_field] = grid_r.read_field_as_string(form_code_field, grid)
-    else:
-        field_data[grid_i.Field.LAST_NAME] = last_name
-        for field in grid_i.Field:
-            if field is not grid_i.Field.LAST_NAME:
-                field_data[field] = grid_r.read_field_as_string(field, grid)
-
+    # Get the answers for questions
     answers = [
         grid_r.read_answer_as_string(i, grid)
         for i in range(grid_i.NUM_QUESTIONS)
     ]
 
+    field_data: typing.Dict[grid_i.Field, str] = {}
+
+    # Read the last name. If it indicates this exam is a key, treat it as such
+    last_name = grid_r.read_field_as_string(grid_i.Field.LAST_NAME, grid)
     if last_name == grid_i.KEY_LAST_NAME:
+        form_code_field = grid_i.Field.TEST_FORM_CODE
+        field_data[form_code_field] = grid_r.read_field_as_string(
+            form_code_field, grid)
         keys_results.add(field_data, answers)
+        print(f"Processed '{image_path.name}' successfully as the key for form code {field_data[form_code_field]}.")
     else:
+        field_data[grid_i.Field.LAST_NAME] = last_name
+        for field in grid_i.Field:
+            # Avoids re-reading the last name value to save some time
+            if field is not grid_i.Field.LAST_NAME:
+                field_data[field] = grid_r.read_field_as_string(field, grid)
         answers_results.add(field_data, answers)
-    print(f"Processed '{image_path.name}' successfully.")
+        print(f"Processed '{image_path.name}' successfully.")
+
 
 answers_results.save(output_directory / "results.csv")
 if (keys_results.row_count != 0):
