2b5d4feeef922fdf762759893ae25ce65d3ca8c7
Ian Sanders <iansanders@mail.usf.edu>
2019-11-18 23:49:42 -0500

    Show errors in user interface

--- code/bubble_sheet_reader.py
+++ code/bubble_sheet_reader.py
@@ -1,5 +1,6 @@
 import time
 import typing
+import textwrap
 
 import corner_finding
 import data_exporting
@@ -24,76 +25,79 @@ keys_file = user_input.keys_file
 arrangement_file = user_input.arrangement_file
 sort_results = user_input.sort_results
 
-progress = user_interface.ProgressTracker(user_input.root,
-                                          len(image_paths))
+progress = user_interface.ProgressTracker(user_input.root, len(image_paths))
+try:
+    for image_path in image_paths:
+        progress.set_status(f"Processing '{image_path.name}'.")
+        image = image_utils.get_image(image_path)
+        prepared_image = image_utils.prepare_scan_for_processing(image)
 
-for image_path in image_paths:
-    progress.set_status(f"Processing '{image_path.name}'.")
-    image = image_utils.get_image(image_path)
-    prepared_image = image_utils.prepare_scan_for_processing(image)
+        # Find the corners, skipping the image on failure
+        try:
+            corners = corner_finding.find_corner_marks(prepared_image)
+        except corner_finding.CornerFindingError:
+            progress.set_status(
+                f"Error with '{image_path.name}': couldn't find corners. Skipping..."
+            )
+            time.sleep(1)
+            continue
 
-    # Find the corners, skipping the image on failure
-    try:
-        corners = corner_finding.find_corner_marks(prepared_image)
-    except corner_finding.CornerFindingError:
-        progress.set_status(
-            f"Error with '{image_path.name}': couldn't find corners. Skipping..."
-        )
-        time.sleep(1)
-        continue
+        # Establish a grid
+        grid = grid_r.Grid(corners, grid_i.GRID_HORIZONTAL_CELLS,
+                           grid_i.GRID_VERTICAL_CELLS, prepared_image)
+        # Calculate the fill threshold
+        threshold = grid_r.calculate_bubble_fill_threshold(grid)
 
-    # Establish a grid
-    grid = grid_r.Grid(corners, grid_i.GRID_HORIZONTAL_CELLS,
-                       grid_i.GRID_VERTICAL_CELLS, prepared_image)
-    # Calculate the fill threshold
-    threshold = grid_r.calculate_bubble_fill_threshold(grid)
+        # Get the answers for questions
+        answers = [
+            grid_r.read_answer_as_string(i, grid, multi_answers_as_f,
+                                         threshold)
+            for i in range(grid_i.NUM_QUESTIONS)
+        ]
 
-    # Get the answers for questions
-    answers = [
-        grid_r.read_answer_as_string(i, grid, multi_answers_as_f, threshold)
-        for i in range(grid_i.NUM_QUESTIONS)
-    ]
+        field_data: typing.Dict[grid_i.Field, str] = {}
 
-    field_data: typing.Dict[grid_i.Field, str] = {}
+        # Read the last name. If it indicates this exam is a key, treat it as such
+        last_name = grid_r.read_field_as_string(grid_i.Field.LAST_NAME, grid,
+                                                threshold)
+        if last_name == grid_i.KEY_LAST_NAME:
+            form_code_field = grid_i.Field.TEST_FORM_CODE
+            field_data[form_code_field] = grid_r.read_field_as_string(
+                form_code_field, grid, threshold)
+            keys_results.add(field_data, answers)
+        else:
+            field_data[grid_i.Field.LAST_NAME] = last_name
+            for field in grid_i.Field:
+                # Avoids re-reading the last name value to save some time
+                if field is not grid_i.Field.LAST_NAME:
+                    field_data[field] = grid_r.read_field_as_string(
+                        field, grid, threshold)
+            answers_results.add(field_data, answers)
+        progress.step_progress()
 
-    # Read the last name. If it indicates this exam is a key, treat it as such
-    last_name = grid_r.read_field_as_string(grid_i.Field.LAST_NAME, grid,
-                                            threshold)
-    if last_name == grid_i.KEY_LAST_NAME:
-        form_code_field = grid_i.Field.TEST_FORM_CODE
-        field_data[form_code_field] = grid_r.read_field_as_string(
-            form_code_field, grid, threshold)
-        keys_results.add(field_data, answers)
+    answers_results.clean_up("G" if empty_answers_as_g else "")
+    answers_results.save(output_folder / "results.csv", sort_results)
+
+    success_string = "✔️ All exams processed and saved to 'results.csv'.\n"
+
+    if keys_file:
+        keys_results.add_file(keys_file)
+
+    if (keys_results.row_count != 0):
+        keys_results.save(output_folder / "keys.csv", sort_results)
+        success_string += "✔️ All keys processed and saved to 'keys.csv'.\n"
+        scores = scoring.score_results(answers_results, keys_results)
+        scores.save(output_folder / "scores.csv", sort_results)
+        success_string += "✔️ All scored results processed and saved to 'scores.csv'."
+        if arrangement_file:
+            data_exporting.save_reordered_version(
+                scores, arrangement_file, output_folder / "reordered.csv")
+            success_string += "✔️ Reordered results saved to 'reordered.csv'."
     else:
-        field_data[grid_i.Field.LAST_NAME] = last_name
-        for field in grid_i.Field:
-            # Avoids re-reading the last name value to save some time
-            if field is not grid_i.Field.LAST_NAME:
-                field_data[field] = grid_r.read_field_as_string(
-                    field, grid, threshold)
-        answers_results.add(field_data, answers)
-    progress.step_progress()
+        success_string += "No exam keys were found, so no scoring was performed."
 
-answers_results.clean_up("G" if empty_answers_as_g else "")
-answers_results.save(output_folder / "results.csv", sort_results)
-
-success_string = "✔️ All exams processed and saved to 'results.csv'.\n"
-
-if keys_file:
-    keys_results.add_file(keys_file)
-
-if (keys_results.row_count != 0):
-    keys_results.save(output_folder / "keys.csv", sort_results)
-    success_string += "✔️ All keys processed and saved to 'keys.csv'.\n"
-    scores = scoring.score_results(answers_results, keys_results)
-    scores.save(output_folder / "scores.csv", sort_results)
-    success_string += "✔️ All scored results processed and saved to 'scores.csv'."
-    if arrangement_file:
-        data_exporting.save_reordered_version(scores, arrangement_file,
-                                              output_folder / "reordered.csv")
-        success_string += "✔️ Reordered results saved to 'reordered.csv'."
-else:
-    success_string += "No exam keys were found, so no scoring was performed."
-
-progress.set_status(success_string, False)
+    progress.set_status(success_string, False)
+except Exception as e:
+    wrapped_err = "\n".join(textwrap.wrap(str(e), 70))
+    progress.set_status(f"Error: {wrapped_err}", False)
 progress.show_exit_button_and_wait()
