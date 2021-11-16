6d90303f488c69763b48119a3ef23cd8ea48a34c
N1m6 <37183754+N1m6@users.noreply.github.com>
2021-11-16 18:14:27 +0200

    Add a CLI (#58)
    * Create process_input.py to wrap input processing from main.py.
    Rename main.py to main_gui.py and modify it to call new process_input module.
    Create new main.py which implement command line interface.
    
    * Change main.py to main_gui.py in fifth instruction in build_instructions.md.
    Adding instructions how to run cli or gui from source to readme.md.
    
    * Update code/main.py
    
    Co-authored-by: Ian Sanders <iansan5653@gmail.com>
    
    * Update code/main.py
    
    Co-authored-by: Ian Sanders <iansan5653@gmail.com>
    
    * Updating main.py
    
    Removing name and usage parameters to ArgumentParser call
    
    * Little doc string
    
    Co-authored-by: Ian Sanders <iansan5653@gmail.com>

---
+++ code/main_gui.py
@@ -0,0 +1,35 @@
+import file_handling
+import grid_info as grid_i
+import user_interface
+import sys
+from process_input import process_input
+
+user_input = user_interface.MainWindow()
+if (user_input.cancelled):
+    sys.exit(0)
+
+input_folder = user_input.input_folder
+image_paths = file_handling.filter_images(
+    file_handling.list_file_paths(input_folder))
+output_folder = user_input.output_folder
+multi_answers_as_f = user_input.multi_answers_as_f
+empty_answers_as_g = user_input.empty_answers_as_g
+keys_file = user_input.keys_file
+arrangement_file = user_input.arrangement_map
+sort_results = user_input.sort_results
+output_mcta = user_input.output_mcta
+debug_mode_on = user_input.debug_mode
+form_variant = grid_i.form_150q if user_input.form_variant == user_interface.FormVariantSelection.VARIANT_150_Q else grid_i.form_75q
+progress_tracker = user_input.create_and_pack_progress(maximum=len(image_paths))
+
+process_input(image_paths,
+              output_folder,
+              multi_answers_as_f,
+              empty_answers_as_g,
+              keys_file,
+              arrangement_file,
+              sort_results,
+              output_mcta,
+              debug_mode_on,
+              form_variant,
+              progress_tracker)
